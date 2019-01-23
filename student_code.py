91f7349a31aea76e332e1f1018ca7bce03495462
AuroraChris <x515034598@yahoo.com>
2019-01-23 15:43:30 -0600

    hw1

--- student_code.py
+++ student_code.py
@@ -19,11 +19,12 @@ class KnowledgeBase(object):
 
     def kb_assert(self, fact):
         """Assert a fact or rule into the KB
-
+        
         Args:
             fact (Fact or Rule): Fact or Rule we're asserting in the format produced by read.py
         """
-        print("Asserting {!r}".format(fact))
+        if fact.name == "fact":
+            self.facts.append(fact)
         
     def kb_ask(self, fact):
         """Ask if a fact is in the KB
@@ -34,4 +35,10 @@ class KnowledgeBase(object):
         Returns:
             ListOfBindings|False - ListOfBindings if result found, False otherwise
         """
-        print("Asking {!r}".format(fact))
+        binding = []
+        for i in self.facts:
+            match_res = match(fact.statement, i.statement, None)
+            if match_res != 0:
+                binding.append(match_res)
+
+        return False if not binding else binding
